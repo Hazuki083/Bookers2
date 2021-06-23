ba422bc26f4dbd69473c5d55a1b343bcde30f64e
Hazuki083 <ha882ki@gmail.com>
2021-06-23 13:42:23 +0000

    bookページ編集中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,25 +6,43 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
 
   end
 
   def edit
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def show
     @user = current_user
     @book = Book.new
-    @book = Book.find(params[:id])
+    @books = Book.find(params[:id])
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  def destroy
 
+  def destroy
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,30 @@
 class UsersController < ApplicationController
    def new
-    @book = Book.new
-   end
-
-   def create
-
+       @book = Book.new
    end
 
    def index
-    @user = current_user
-    @book = Book.new
-    @books = Book.all
-
+       @user = current_user
+       @book = Book.new
+       @books = Book.all
    end
 
    def show
+       @user = User.find(params[:id])
+       @books = @user.books.all
+       @book = Book.new
    end
 
    def edit
+       @user = User.find(params[:id])
    end
+
+   def update
+   end
+
+ private
+
+  def user_params
+      params.require(:user).permit(:title, :profile_image, :introduction)
+  end
 end
